377b1d350d937f2b61883bd45e49bc8e62cf7614
qifan.wu <qifan.wu@rakuten.com>
2018-01-14 18:46:09 +0900

    Add new pin on long press. Add segue to show detail view controller.

--- VirtualTourist/ViewController/TravelLocationsMapViewController.swift
+++ VirtualTourist/ViewController/TravelLocationsMapViewController.swift
@@ -6,12 +6,56 @@
 //
 
 import UIKit
+import MapKit
 
 class TravelLocationsMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(addPin))
+        gestureRecognizer.delegate = self
+        mapView.addGestureRecognizer(gestureRecognizer)
+        mapView.delegate = self
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAlbum" {
+            
+        }
+    }
 }
 
+extension TravelLocationsMapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = false
+            pinView!.pinTintColor = .red
+        } else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        performSegue(withIdentifier: "showAlbum", sender: nil)
+    }
+}
+
+extension TravelLocationsMapViewController: UIGestureRecognizerDelegate {
+    @objc func addPin(gestureReconizer: UILongPressGestureRecognizer) {
+        let location = gestureReconizer.location(in: mapView)
+        let coordinate = mapView.convert(location,toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+    }
+}
